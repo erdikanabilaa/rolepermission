e9e9667674cf761b33b4e8fdb399c9dd9e9e2c17
erdikanabilaa <159235785+erdikanabilaa@users.noreply.github.com>
2024-05-30 21:33:06 +0700

    Initial commit

---
+++ resources/views/manageHome.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        11
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-body">
+                    You are a Manager User.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
